5097e12589542053230bb878a7962b419bd200c3
Md Sharif <srka780@gmail.com>
2024-01-18 16:47:30 +0600

    banner sec complate for pc layout

--- src/pages/home/header/banner/BannerLeft.jsx
+++ src/pages/home/header/banner/BannerLeft.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 import { FaArrowRightLong } from "react-icons/fa6";
 const BannerLeft = () => {
   return (
-    <div className=" w-auto h-full bg-slate-600 ">
+    <div className=" w-auto h-full  ">
       <div className=" relative h-full w-[537px] ">
-        <div className=" flex flex-col pl-1 pr-24 justify-center gap-8 absolute top-1/2 rounded-tr-[50px] -translate-y-1/2  w-[720px] bg-red-600 ">
+        <div className="absolute flex flex-col pl-1 pr-24 justify-center gap-8  top-1/2 rounded-tr-[50px] -translate-y-1/2  w-[720px] bg-white ">
           <h2 className=" font-semibold text-[#16205F] font-Poppins text-6xl leading-tight tracking-wide ">
             Building dev team, Quickly and Affordably
           </h2>
@@ -14,8 +14,11 @@ const BannerLeft = () => {
             or scale to different needs quickly.
           </p>
           <Link to={""}>
-            <button className=" flex justify-center items-center gap-2 rounded-lg bg-gradient-to-tr p-4 px-8 from-[#f90] to-[#F2CB00] text-sm text-white font-Poppins font-medium ">
-              Build a team <span className=" text-xl " ><FaArrowRightLong></FaArrowRightLong></span>
+            <button className=" relative transition group duration-500 ease-in-out shadow-sm shadow-[#ff99007a] flex justify-center items-center  gap-2 rounded-lg bg-gradient-to-r hover:bg-gradient-to-br p-4 from-[#f90]  to-[#F2CB00] text-sm text-white font-Poppins font-medium ">
+              <h2 className=" px-4 pr-10 ">Build a team</h2>
+              <span className=" absolute transition ease-in-out top-1/2 -translate-y-1/2 group-hover:right-[24px] right-[28px] text-xl  ">
+                <FaArrowRightLong></FaArrowRightLong>
+              </span>
             </button>
           </Link>
         </div>
--- src/pages/home/header/banner/BannerRight.jsx
+++ src/pages/home/header/banner/BannerRight.jsx
@@ -2,7 +2,7 @@ import banner from "./../../../../assets/banner/bannerimg.jpg"
 
 const BannerRight = () => {
   return (
-    <div className=" w-auto  h-full bg-slate-600 flex justify-center items-center ">
+    <div className=" w-auto  h-full  flex justify-center items-center ">
       <img
         src={banner} 
         alt="banner"
