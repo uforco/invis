80e51561396918b0cd94b311f15607b2bf751cdc
Md Sharif <srka780@gmail.com>
2024-01-19 17:42:43 +0600

    container overflow use to deploy by pc user

--- src/layout/widthControl/WidthControl.jsx
+++ src/layout/widthControl/WidthControl.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 const WidthControl = ({ children }) => {
-  return <div className=" w-auto max-w-screen-2xl mx-auto ">{children}</div>;
+  return <div className=" w-auto max-w-screen-2xl mx-auto overflow-hidden ">{children}</div>;
 };
 
 WidthControl.propTypes = {
