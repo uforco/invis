ff7a64f4274856989cfe9a0574b992f8bef0dac7
Md Sharif <srka780@gmail.com>
2024-01-22 02:47:20 +0600

    location set with map4

--- src/component/locationMap/Gmap.jsx
+++ src/component/locationMap/Gmap.jsx
@@ -1,6 +1,13 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
+
 const Gmap = () => {
+  const marklogo = new Icon({
+    iconUrl: "https://i.ibb.co/8cTBjfY/your-location.png",
+    iconSize: [38, 38],
+  });
+
   return (
     <MapContainer
       center={[23.822651, 90.393418]}
@@ -11,7 +18,7 @@ const Gmap = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[23.822654, 90.393418]}>
+      <Marker position={[23.822654, 90.393418]} icon={marklogo}>
         <Popup>Invis | ECB Chattar</Popup>
       </Marker>
     </MapContainer>
