b4ee6bdbbe1d81a1909642dc1fdd579e1d0f027e
Md Sharif <srka780@gmail.com>
2024-01-28 00:56:46 +0600

    mobile view3

--- src/component/locationMap/Gmap.jsx
+++ src/component/locationMap/Gmap.jsx
@@ -1,27 +1,28 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-// import { Icon } from "leaflet";
+import { Icon } from "leaflet";
 
 const Gmap = () => {
-  // const marklogo = new Icon({
-  //   iconUrl: "https://i.ibb.co/HN6s3KG/location-1.png",
-  //   iconSize: [56, 56],
-  // });
+  const marklogo = new Icon({
+    iconUrl: "https://i.ibb.co/HN6s3KG/location-1.png",
+    iconSize: [40, 46],
+  });
 
   return (
     <div className=" relative w-full h-full " >
       <div className=" z-0 w-full bg-blue-gray-400 h-full " >
         <MapContainer
           center={[43.919926, -80.069481]}
-          zoom={15}
+          zoom={14}
           style={{ height: "100%", width: "100%", zIndex: "2" }}
+          scrollWheelZoom={false}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[43.92010, -80.069400]} >
-            <Popup>311 Marshall Crescent, Orangeville, ON L9W 4Y5, Canada</Popup>
+          <Marker position={[43.92100, -80.069460]} icon={marklogo}>
+            <Popup >311 Marshall Crescent, Orangeville, ON L9W 4Y5, Canada</Popup>
           </Marker>
         </MapContainer>
       </div>
